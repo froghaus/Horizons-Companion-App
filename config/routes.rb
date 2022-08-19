1363be5d47babe946f8383f43ee54e2e079eac97
Nicholas Adams <nicholasadams@Nicholass-MBP.fios-router.home>
2022-08-19 11:40:25 -0400

    rough draft complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/characters", to: "homes#show"
+  get "/characters/new", to: "homes#show"
+  get "/characters/:id", to: "homes#show"
+
+  namespace :api do
+    namespace :v1 do
+      resources :characters, only: [:index, :show, :new, :create]
+    end
+  end
+
 end
